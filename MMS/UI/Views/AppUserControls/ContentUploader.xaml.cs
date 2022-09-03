812859e26763b78c5e409383b559d483034ef401
d3fault1 <rn2osystems@gmail.com>
2022-09-04 05:32:41 +0600

    Added some user controls and component styles

---
+++ MMS/UI/Views/AppUserControls/ContentUploader.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MMS.UI.Views.AppUserControls
+{
+    /// <summary>
+    /// Interaction logic for ContentUploader.xaml
+    /// </summary>
+    public partial class ContentUploader : UserControl
+    {
+        public ContentUploader()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UploadClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MMS/UI/Views/AppUserControls/DateRange.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MMS.UI.Views.AppUserControls
+{
+    /// <summary>
+    /// Interaction logic for DateRange.xaml
+    /// </summary>
+    public partial class DateRange : UserControl
+    {
+        public delegate void DateRangeChangedEventHandler(object sender, DateRangeChangedEventArgs e);
+        public event DateRangeChangedEventHandler DateRangeChanged;
+
+        private bool Applied;
+        public DateRange()
+        {
+            InitializeComponent();
+        }
+
+        private void EnabledChecked(object sender, RoutedEventArgs e)
+        {
+            Applied = true;
+            OnDateRangeChanged();
+        }
+
+        private void EnabledUnchecked(object sender, RoutedEventArgs e)
+        {
+            Applied = false;
+            OnDateRangeChanged();
+        }
+
+        private void FromDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            OnDateRangeChanged();
+        }
+
+        private void ToDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            OnDateRangeChanged();
+        }
+
+        private void OnDateRangeChanged()
+        {
+            DateRangeChangedEventArgs eventArgs = new DateRangeChangedEventArgs(Applied, From.SelectedDate, To.SelectedDate);
+            DateRangeChanged?.Invoke(this, eventArgs);
+        }
+    }
+
+    public struct DateRangeChangedEventArgs
+    {
+        public bool IsEnabled { get; }
+        public DateTime? FromDate { get; }
+        public DateTime? ToDate { get; }
+        public DateRangeChangedEventArgs(bool enabled, DateTime? from, DateTime? to)
+        {
+            IsEnabled = enabled;
+            FromDate = from;
+            ToDate = to;
+        }
+    }
+}
